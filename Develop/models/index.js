b7109a0474cb44e1b596c0e4816d32e628ad837e
Hayden Kiltoff <hayden060@gmail.com>
2022-05-27 07:55:25 -0700

    updated api routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,49 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'tag'
+});
 
 // Categories have many Products
+Category.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product_tag'
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Tag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Product,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product'
+});
 
 module.exports = {
   Product,
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,11 +6,41 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      // JOIN with travellers, using the Trip through table
+      include: [{ model: Product, through: ProductTag, as: 'Product_ProductTag' }]
+    });
+
+    if (!tagdataData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      // JOIN with locations, using the Trip through table
+      include: [{ model: Product, through: ProductTag, as: 'Product_ProductTag' }]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
